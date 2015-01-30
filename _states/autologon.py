6daa1185a763029b37b14afc42fd7f1058a64dff
Freddy Tan <beelit94@gmail.com>
2015-01-30 16:13:47 +0800

    format error in otology state

--- _states/autologon.py
+++ _states/autologon.py
@@ -1,12 +1,14 @@
 import salt.exceptions
 
+
 def enable_user(username):
-	ret = {'username': username, 'changes': {}, 'result': False, 'comment': '', 'key': ''}
+    ret = {'username': username, 'changes': {}, 'result': False, 'comment': '', 'key': ''}
 
-	# todo, check user name is not in the list
+    # todo check user name is not in the list
 
-    # Check the current state of the system. Does anything need to change?
-    key = __salt__['reg.read_key']('HKEY_LOCAL_MACHINE', 'SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon', 'AutoAdminLogon')
+    key = __salt__['reg.read_key']('HKEY_LOCAL_MACHINE',
+                                   'SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon',
+                                   'AutoAdminLogon')
 
     ret['key'] = key
 
