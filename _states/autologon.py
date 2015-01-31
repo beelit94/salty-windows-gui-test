06668301a053085862df70af8f579bace31461cc
Freddy Tan <beelit94@gmail.com>
2015-01-31 23:53:07 +0800

    add log

--- _states/autologon.py
+++ _states/autologon.py
@@ -1,4 +1,7 @@
 import salt.exceptions
+import logging
+
+log = logging.getLogger(__name__)
 
 
 def enable_user(name, username, password):
@@ -9,17 +12,19 @@ def enable_user(name, username, password):
     auto_admin_logon = __salt__['reg.read_key']('HKEY_LOCAL_MACHINE',
                                                 'SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon',
                                                 'AutoAdminLogon')
+    log.info('original AutoAdminLogon: %s' % auto_admin_logon)
     default_user = __salt__['reg.read_key']('HKEY_LOCAL_MACHINE',
                                             'SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon',
                                             'DefaultUserName')
+    log.info('original DefaultUserName: %s' % default_user)
     default_domain_name = __salt__['reg.read_key']('HKEY_LOCAL_MACHINE',
                                                    'SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon',
                                                    'DefaultDomainName')
-
+    log.info('original DefaultDomainName: %s' % default_domain_name)
     default_password = __salt__['reg.read_key']('HKEY_LOCAL_MACHINE',
                                                 'SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon',
                                                 'DefaultPassword')
-
+    log.info('original DefaultPassword: %s' % default_password)
     computer_name = __salt__['system.get_computer_name']()
 
     if auto_admin_logon == str(1) \
@@ -42,25 +47,30 @@ def enable_user(name, username, password):
     # not exist, the create key module will set key instead fo create key
     # if a key is already exist
     try:
-        __salt__['reg.create_key']('HKEY_LOCAL_MACHINE',
-                                   'SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon',
-                                   'AutoAdminLogon',
-                                   '1')
+        result = __salt__['reg.create_key']('HKEY_LOCAL_MACHINE',
+                                            'SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon',
+                                            'AutoAdminLogon',
+                                            '1')
+        log.info('AutoAdminLogon created' + str(result))
 
-        __salt__['reg.create_key']('HKEY_LOCAL_MACHINE',
+        result = __salt__['reg.create_key']('HKEY_LOCAL_MACHINE',
                                    'SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon',
                                    'DefaultUserName',
                                    username)
+        log.info('DefaultUserName' + str(result))
 
-        __salt__['reg.create_key']('HKEY_LOCAL_MACHINE',
+        result = __salt__['reg.create_key']('HKEY_LOCAL_MACHINE',
                                    'SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon',
                                    'DefaultDomainName',
                                    computer_name)
+        log.info('DefaultDomainName' + str(result))
 
-        __salt__['reg.create_key']('HKEY_LOCAL_MACHINE',
+        result = __salt__['reg.create_key']('HKEY_LOCAL_MACHINE',
                                    'SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon',
                                    'DefaultPassword',
                                    password)
+        log.info('DefaultPassword' + str(result))
+
     except Exception as err:
         ret['result'] = False
         ret['comment'] = str(err)
